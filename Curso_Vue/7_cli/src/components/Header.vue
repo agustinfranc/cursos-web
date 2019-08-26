bdca42b4a451d905ef90003240e0e8d676941ff7
Agustin <agustin.amato96@gmail.com>
2019-08-26 02:06:35 -0300

    automatic commit

---
+++ Curso_Vue/7_cli/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        <img alt="Vue logo" src="@/assets/logo.png"> <!-- con @ me posiciono en src -->
+        <h1>Frutas</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
---
+++ Curso_Vue/7_cli/src/components/Lista.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li v-for="(item, index) of frutas" :key="item.id" @click="aumentar(index)" class="list-group-item d-flex justify-content-between align-items-center">
+        {{item.nombre}}
+        <span class="badge badge-secondary badge-pill">{{item.cantidad}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  name: "Lista",
+  computed: {
+      ...mapState(['frutas']),   // obtengo array frutas
+  },
+  methods: {
+      ...mapMutations(['aumentar'])
+  },
+};
+</script>
