be645239e542bd7b0988674cf14c7ce15cc060b2
Agustin Amato <agustin.amato@protonmail.com>
2020-02-17 12:02:38 -0300

    feat termino login

--- Curso_Vue/10_firebase-auth/src/views/Login.vue
+++ Curso_Vue/10_firebase-auth/src/views/Login.vue
@@ -3,18 +3,18 @@
         <h1>Ingreso de usuarios</h1>
         <form @submit.prevent="ingresarUsuario({email: $v.email.$model, pass: $v.pass.$model})">     <!-- como el nombre de la propiedad es igual a la variable me ahorro escribir {email: email, pass: pass} -->
             <input type="email" class="form-control my-3" placeholder="Ingresa un email" v-model="$v.email.$model">
-            <small class="text-danger d-block" v-if="!$v.email.required">Campo requerido</small>
-            <small class="text-danger d-block" v-if="$v.email.$invalid">Ingrese un email valido</small>
+            <small class="text-danger d-block my-3" v-if="!$v.email.required">Campo requerido</small>
+            <small class="text-danger d-block my-3" v-if="$v.email.$invalid">Ingrese un email valido</small>
             
             <input type="password" class="form-control my-3" v-model="$v.pass.$model" placeholder="Ingresa una contraseña">
-            <small class="text-danger d-block" v-if="!$v.pass.required">Campo requerido</small>
-            <small class="text-danger d-block" v-if="$v.pass.minLength">Minimo 6 caracteres</small>
+            <small class="text-danger d-block my-3" v-if="!$v.pass.required">Campo requerido</small>
+            <small class="text-danger d-block my-3" v-if="$v.pass.minLength">Minimo 6 caracteres</small>
             
             <button type="submit" class="btn btn-primary" :disabled="$v.$invalid">Acceder</button>
         </form>
         <small class="text-danger d-block"></small>
-        <p>{{ error }}</p>
-        <p>{{ $v }}</p>
+        <p class="my-3">{{ error }}</p>
+        <!-- <p>{{ $v }}</p> -->
     </div>
 </template>
 
