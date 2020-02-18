63ac074280b02a3c9fa2b6e8cdbb0d6b8cc215d5
Agustin <agustin.amato96@gmail.com>
2020-02-18 18:02:18 -0300

    commit

--- Curso_Vue/12_calendario/src/views/Home.vue
+++ Curso_Vue/12_calendario/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-row>
+    <v-col cols=12>
+      <h1>Calendario</h1>
+    </v-col>
+    <v-col>
+      <v-card>
+        <v-date-picker v-model="fecha" full-width></v-date-picker>
+      </v-card>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-center">
+          {{ fecha }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      fecha: '',
+    }
   }
 }
 </script>
