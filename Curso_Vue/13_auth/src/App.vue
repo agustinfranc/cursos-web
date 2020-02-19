afb269ac99473aa1536deca361f79460ae8917d6
Agustin Amato <agustin.amato@protonmail.com>
2020-02-19 15:33:34 -0300

    commit

--- Curso_Vue/13_auth/src/App.vue
+++ Curso_Vue/13_auth/src/App.vue
@@ -25,33 +25,19 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+
+      <router-view/>
+
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
