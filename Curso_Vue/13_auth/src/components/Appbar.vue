622f1e8221688b9937a5af1f61b5213810ff1652
Agustin Amato <agustin.amato96@gmail.com>
2020-03-10 00:47:28 -0300

    commit

---
+++ Curso_Vue/13_auth/src/components/Appbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        <router-link :to="{name: 'Home'}">Title</router-link>
+      </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "Appbar",
+
+  data: () => ({
+    drawer: null,
+  }),
+  methods: {
+    ...mapActions(["cerrarSesion"])
+  }
+};
+</script>
